d93ff0a2dfaec1bae2e56157ba9e26d86061debb
Mateo Balcorta <misterrattail@gmail.com>
2017-07-02 20:19:41 -0700

    added 'x' onto each square upon onClick

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
+
+$(document).ready(function(){
 <!-- must create 3 * 3 square dynamically created-->
-var total_boxes = [0, 1, 2, 3, 4, 5, 6, 7, 8]; 
-var box_Totals = []; 
-
-_.each(total_boxes, function(boxNumber){
-  $(".container").append("<div class=box id="+boxNumber+"></div>")
-});
-
-var arrayOfRowEnd = 
-  _.filter(total_boxes, function(num){ 
-    var numPosition = num + 1; 
-    if(numPosition % 3 === 0){
-      return num
-     }  
+  var total_boxes = [1, 2, 3, 4, 5, 6, 7, 8, 9]; 
+  var box_Totals = []; 
+  _.each(total_boxes, function(boxNumber){
+    $(".containerBox").append("<div class=box id="+boxNumber+"></div>")
+    box_Totals.push({
+      box: boxNumber,
+      active: false,
+      selector: null    
+    })
   });
 
-_.each(arrayOfRowEnd, function(boxNumber){
-console.log(boxNumber)
-    $("#"+boxNumber).css({"backgroundColor": "black"})
-   <!--  $("#"+boxNumber).css({"clear": "right"}) -->
+  $('.box').click(function(){
+     $(this).append('<div class=xSpot>X</div>');
+  })
 });
 
