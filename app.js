123977bc4d9c15ee8076896a92671769d9b7abe7
Mateo Balcorta <misterrattail@gmail.com>
2017-07-09 17:46:29 -0700

    game working with two players

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var player_one = "X";
 var player_two = "O"
 var firstPlayer = true;
 var winnerFound = false; 
-
+var game_started = false; 
  // must create 3 * 3 square dynamically created
 var total_boxes = [0, 1, 2, 3, 4, 5, 6, 7, 8]; 
 var box_Totals = [];
@@ -15,17 +15,42 @@ var winning_combo_array_o = [[0, 1, 2], [3, 4, 5], [6, 7, 8],[2, 4, 6],[1, 4, 7]
 
 
   // creates each box with an object  
-  _.each(total_boxes, function(boxNumber){
-    $(".containerBox").append("<div class=box id="+boxNumber+"></div>")
-    box_Totals.push({
-      box: boxNumber,
-      active: false,
-      selector: null    
-    })
-  });
+  function createBoxes(){
+
+    if($('.containerBox').length < 1){
+      $('.container').append('<div class=containerBox></div>');
+       _.each(total_boxes, function(boxNumber){
+   
+      $(".containerBox").append("<div class=box id="+boxNumber+"></div>")
+      box_Totals.push({
+        box: boxNumber,
+        active: false,
+        selector: null    
+       })
+      });
+       console.log('!!!!!!!!! ', box_Totals)
+    } else {
+
+     _.each(total_boxes, function(boxNumber){
+   
+      $(".containerBox").append("<div class=box id="+boxNumber+"></div>")
+        box_Totals.push({
+        box: boxNumber,
+        active: false,
+        selector: null    
+       })
+      });
+    } 
+}; 
+
+//initally sets up boxes for game
+if(!game_started) {
+  createBoxes()
+  game_started = true; 
+};
 
 // when box clicked it places an x or an o 
-  $('.box').click(function(){
+ $(document).on("click", ".box", function(){
   var boxNotOccuplied = box_Totals[this.id].active;
     if(!boxNotOccuplied){
       if(firstPlayer){
@@ -56,14 +81,11 @@ function updatesWinningArray(onlyPlayerOneBoxes, array_tracker){
     
         if(array_tracker[i].indexOf(onlyPlayerOneBoxes[j]) > -1) {
 
-         array_tracker[i].splice(array_tracker[i].indexOf(onlyPlayerOneBoxes[j]), 1)
-        
+         array_tracker[i].splice(array_tracker[i].indexOf(onlyPlayerOneBoxes[j]), 1)        
         } 
-        console.log('winning_combo_array', array_tracker)
-       };
+      };
     };
-
-}
+  }
 
 function onlyOnePlayerBoxType(allBoxesArray, playerType){
   return _.filter(allBoxesArray, function(eachBoxObj){
@@ -104,12 +126,65 @@ function checkIfWinner(player, array_tracker){
   }
 }; 
 
+//resets game when winner found
+function resetGame(player){
+
+function popUpConfirmBox(player){
+
+ $('.container').prepend(
+  '<div id="dialog_box" style= "display: none;"></div>');
+
+$('body').css({ 'z-index':'1', 'background-color': 'white', 'opacity': '1' });
+
+  $('#dialog_box').dialog({
+      title: 'Congrats! '+player+' player you won! Would you like to play again?',
+      width: 500,
+      height: 200,
+      modal: true,
+      resizable: false,
+      draggable: false,
+      buttons: [
+      {
+      text: 'no thanks',
+      click: function() {
+          $('#dialog_box').hide();
+          $('body').hide();
+          $('body').css({'background-image' : 'url(cat.jpg)',
+      'background-repeat': 'no-repeat'});
+        }
+    
+      },
+      {
+      text: 'sure',
+      click: function(){
+        $('#dialog_box').hide();
+        $('.ui-dialog-content').dialog('close'); 
+        winning_combo_array_x = [[0, 1, 2], [3, 4, 5], [6, 7, 8],[2, 4, 6],[1, 4, 7], [2, 5, 8],[0, 3, 6], [0, 4, 8]]; 
+
+        winning_combo_array_o = [[0, 1, 2], [3, 4, 5], [6, 7, 8],[2, 4, 6],[1, 4, 7], [2, 5, 8],[0, 3, 6], [0, 4, 8]]; 
+        box_Totals = [];
+        winnerFound = false;
+        firstPlayer = true;
+        $('.containerBox').detach();
+          createBoxes()
+        }
+      }]
+    })
+  }
+  popUpConfirmBox(player)
+}
+
   // when winner prompt which player is the winner and ask if game would want to be restarted 
 function checkCounter(player, array){
   for (var i = 0; i < array.length; i++) {
     if(array[i].length === 0){
       console.log('we have a winner, congrats ! ', player)
-      winnerFound = true; 
+      winnerFound = true;
+      if(player === 'X'){
+         resetGame('X'); 
+      } else {
+        resetGame('O')
+        }
       }
     };
   };  
