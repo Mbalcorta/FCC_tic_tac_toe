bbf9a90d7e0a61df2bc6ebb36ca56f76e92b7193
Mateo Balcorta <misterrattail@gmail.com>
2017-07-12 13:47:09 -0700

    computer places piece on board randomly

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 
 $(document).ready(function(){
-var player_one = "X";
-var player_two = "O";
-var firstPlayer;
+var player_x = "X";
+var player_o = "O";
 var winnerFound = false; 
 var game_started = false; 
 var chosenPiece;
-var turns = true;
+var playerGoesFirst; 
  // must create 3 * 3 square dynamically created
 var total_boxes = [0, 1, 2, 3, 4, 5, 6, 7, 8]; 
 var box_Totals = [];
@@ -16,39 +15,6 @@ var winning_combo_array_x = [[0, 1, 2], [3, 4, 5], [6, 7, 8],[2, 4, 6],[1, 4, 7]
 
 var winning_combo_array_o = [[0, 1, 2], [3, 4, 5], [6, 7, 8],[2, 4, 6],[1, 4, 7], [2, 5, 8],[0, 3, 6], [0, 4, 8]]; 
 
-//prompt user if they want to be x or o
-  function askUser(){
-     $('.container').prepend(
-  '<div id="dialog_box" style= "display: none;"></div>');
-
-$('body').css({ 'z-index':'1', 'background-color': 'white', 'opacity': '1' });
-  
-     $('#dialog_box').dialog({
-      title: 'Which letter would you like to be?',
-      width: 100,
-      height: 50,
-      modal: true,
-      resizable: true,
-      draggable: false,
-      buttons: [
-      {
-      text: 'O',
-      click: function() {
-        $('#dialog_box').hide();
-        $('.ui-dialog-content').dialog('close'); 
-        chosenPiece = "O";
-        }
-      },
-      {
-      text: 'X',
-      click: function(){
-        $('#dialog_box').hide();
-        $('.ui-dialog-content').dialog('close'); 
-         chosenPiece = "X";
-        }
-      }]
-    })
-  }
 
   // creates each box with an object  
   function createBoxes(){
@@ -83,34 +49,47 @@ if(!game_started) {
   askUser();
 };
 
-//if choosen piece is x then user goes first 
+//prompt user if they want to be x or o
+  function askUser(){
+     $('.container').prepend(
+  '<div id="dialog_box" style= "display: none;"></div>');
 
-function playerPlacePiece(boxClicked, boxClickedId){
-  var boxNotOccuplied = boxClicked;
-  if(!boxNotOccuplied){
-    $('#'+boxClickedId).append('<div class=xSpot>'+
-        player_one+'</div>');
-    updateBoxObject(boxClickedId, player_one);
-    checkIfWinner(player_one, winning_combo_array_x);
+$('body').css({ 'z-index':'1', 'background-color': 'white', 'opacity': '1' });
+  
+     $('#dialog_box').dialog({
+      title: 'Which letter would you like to be?',
+      width: 100,
+      height: 50,
+      modal: true,
+      resizable: true,
+      draggable: false,
+      buttons: [
+      {
+      text: 'O',
+      click: function() {
+        playerGoesFirst = "computer";
+        $('#dialog_box').hide();
+        $('.ui-dialog-content').dialog('close'); 
+        chosenPiece = "O";
+        var xDiv = 'xSpot';
+        computerPlacement(xDiv, winning_combo_array_x, player_x);
+        }
+      },
+      {
+      text: 'X',
+      click: function(){
+        playerGoesFirst = "human"; 
+        $('#dialog_box').hide();
+        $('.ui-dialog-content').dialog('close'); 
+         chosenPiece = "X";
+        }
+      }]
+    })
   }
-}
 
+ //build function that randomly puts x or o piece on the tic tac toe board
+function computerPlacement(divByLetterType, arrayType, playerType){
 
-// when box clicked it places an x or an o 
- $(document).on("click", ".box", function(){
-  if(chosenPiece === "X" && turns){
-      var boxClicked = box_Totals[this.id].active; 
-      var boxClickedId = box_Totals[this.id].box;
-      firstPlayer = 'user';
-      playerPlacePiece(boxClicked, boxClickedId);
-      turns = false; 
-      computerPlacement(); 
-    } 
-  });
-
- //build function that randomly puts o piece on the tic tac toe board
-function computerPlacement(){
-  if(!turns){
   var randomPlacement = _.filter(box_Totals, function(eachBoxObj){
        return !eachBoxObj.active  
       }).map(function(obj){
@@ -120,17 +99,59 @@ function computerPlacement(){
 var randomValue = randomPlacement[Math.floor(randomPlacement.length * Math.random())];
 
   setTimeout(function(){ 
-    $('#'+randomValue).append('<div class=oSpot>'+player_two+'</div>'); 
-    }, 500);
+    $('#'+randomValue).append('<div class='+divByLetterType+'>'+playerType+'</div>'); 
+    }, 1000);
   
   var boxChoosenId = box_Totals[randomValue].box;
-  updateBoxObject(boxChoosenId, player_two);
-  firstPlayer = 'computer'; 
-  checkIfWinner(player_two, winning_combo_array_o );
-  turns = true;
+  updateBoxObject(boxChoosenId, playerType);
+  checkIfWinner(playerType, arrayType);
+  if(!winnerFound){
+    playerGoesFirst = "human"; 
+  }; 
+}
 
+
+//if choosen piece is x then user goes first then computer piece is placed
+
+function playerPlacePiece(boxClicked, boxClickedId, chosenPiece, div, arrayType, marker){
+  var boxNotOccuplied = boxClicked;
+  if(!boxNotOccuplied){
+    $('#'+boxClickedId).append('<div class='+div+'>'+
+        marker+'</div>');
+    updateBoxObject(boxClickedId, marker);
+    checkIfWinner(marker, arrayType);
+
+    if(!winnerFound){
+//opposite marker is chosen for computer to randomly place element
+      if(marker === 'O' && playerGoesFirst === "human"){
+        var xDiv = 'xSpot';
+        computerPlacement(xDiv, winning_combo_array_x, player_x);
+      } else {
+        var oDiv = 'oSpot';
+        computerPlacement(oDiv, winning_combo_array_o, player_o);
+      }
+    }
   }
 }
+
+
+// when box clicked it places an x or an o 
+ $(document).on("click", ".box", function(){
+  console.log(chosenPiece, playerGoesFirst)
+  if(chosenPiece === "O" && playerGoesFirst === "human"){
+       var oDiv = 'oSpot';
+      var oMarker = 'O';  
+      var boxClicked = box_Totals[this.id].active; 
+      var boxClickedId = box_Totals[this.id].box;
+      playerPlacePiece(boxClicked, boxClickedId, chosenPiece, oDiv, winning_combo_array_o, oMarker);
+    } else {
+      var xDiv = 'xSpot'; 
+      var xMarker = 'X';
+      var boxClicked = box_Totals[this.id].active; 
+      var boxClickedId = box_Totals[this.id].box;
+      playerPlacePiece(boxClicked, boxClickedId, chosenPiece, xDiv, winning_combo_array_x, xMarker);
+    }
+  });
 
 
 // updates box object upon every click 
@@ -138,9 +159,7 @@ var randomValue = randomPlacement[Math.floor(randomPlacement.length * Math.rando
       box_Totals[boxNumber].active = true;
 //pushes box number into array to detemine if cats game
       catsGame.push(boxNumber);
-      if(catsGame.length === box_Totals.length){
-        resetGame('no one won')
-        }
+     
       box_Totals[boxNumber].selector = player; 
   };
 
@@ -177,23 +196,11 @@ function checkIfWinner(player, array_tracker){
       return obj.box;
   });
 
-
-  if(firstPlayer === "user" && !winnerFound){
-
-    var onlyPlayerOneBoxes = onlyOnePlayerBoxType(box_Totals, "X")
-
-    if(onlyPlayerOneBoxes.length >= 3){
-
-      updatesWinningArray(onlyPlayerOneBoxes, array_tracker)
-      checkCounter(player, array_tracker); 
-    }; 
-  } else if(!winnerFound){
-    var onlyPlayerTwoBoxes = onlyOnePlayerBoxType(box_Totals, "O")
-
-    if(onlyPlayerTwoBoxes.length >= 3){
-      updatesWinningArray(onlyPlayerTwoBoxes, array_tracker)
-      checkCounter(player, array_tracker); 
-    }; 
+  var onlyOnePlayerBoxes = onlyOnePlayerBoxType(box_Totals, player)
+  
+  if(onlyOnePlayerBoxes.length >= 3){
+    updatesWinningArray(onlyOnePlayerBoxes, array_tracker);
+    checkCounter(player, array_tracker); 
   }
 }; 
 
@@ -242,8 +249,6 @@ $('body').css({ 'z-index':'1', 'background-color': 'white', 'opacity': '1' });
         box_Totals = [];
         catsGame = [];
         winnerFound = false;
-        firstPlayer = "";
-        turns = true; 
         $('.containerBox').detach();
           createBoxes()
           askUser()
@@ -260,12 +265,25 @@ function checkCounter(player, array){
     if(array[i].length === 0){
       winnerFound = true;
       if(player === 'X'){
-         resetGame('X'); 
+        setTimeout(function(){ 
+          resetGame('X')  
+        }, 1000);
+        break;
       } else {
-        resetGame('O')
+        setTimeout(function(){ 
+          resetGame('O')  
+          }, 1000);
+        break;
         }
       } 
     };
+//check if cats game
+ if(catsGame.length === box_Totals.length && !winnerFound){
+    setTimeout(function(){ 
+      resetGame('no one won'); 
+      }, 1000);
+    }
+
   };  
 });
 
